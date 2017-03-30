91477932cbdf1027d65d3312f3d193976e7ed66b
BuildTools <unconfigured@null.spigotmc.org>
2017-03-29 20:34:04 -0400

    Initial commit

---
+++ src/Selection.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Selection {
+
+    public static void main(String[] args){
+
+        boolean inputcorrect1;
+
+        Scanner input = new Scanner(System.in);
+
+        int leona = 1;
+        int rammus = 1;
+        double riven = 0.5;
+        double talon = 0.5;
+        int caitlyn = 0;
+        int lucian = 0;
+
+        System.out.println("Enter 1st champion");
+
+        String first = input.next();
+
+
+        //I want them to input a champion name like leona,
+        //and then define a new double/idk to equal 1 if
+        //they say leona or rammus, which up there have
+        //a value of 1. I want to somehow reference the
+        //ints or doubles based on what they type in.
+
+        //For example, if I type in leona, I want to make
+        //sure the system registers it as 1, but if they input
+        //a name idk how to give that a value.
+
+
+
+
+    }
+
+}
